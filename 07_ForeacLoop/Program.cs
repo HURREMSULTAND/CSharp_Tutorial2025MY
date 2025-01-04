a71fa1e4b747606d4ea7d765a3021b7c8068b26b
HURREMSULTAND <143317089+HURREMSULTAND@users.noreply.github.com>
2025-01-05 01:24:41 +0300

    5.1.25 commit

---
+++ 07_ForeacLoop/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeacLoop
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Foreach Döngüsü
+
+			// Foreach(1;2;3;4)
+
+			//1:Değişken türü
+			//2:Değişken Adı
+			//3:In
+			//4:Liste,Koleksiyon,Dizi
+			//!!!!!! En Önemli Özellik Başlangıç bitiş değeriyle uğraşmaya gerek yok direk yazdırılabilir!!!!!!!!!!!
+
+			//string[] cities = { "milano", "roma", "budapeşte", "ankara", "istanbul", "varşova" };
+
+			//foreach (string x in cities) 
+			//{
+			//Console.WriteLine(x);
+			//}
+
+
+			//Console.ReadLine();
+
+
+
+			//int[] numbers = { 45, 78, 985, 635, 74, 11, 6589, 4125, 2103698 };
+
+			//foreach (int  number in numbers)
+			//{
+			//Console.WriteLine(number);
+			//}
+
+
+			//int[] numbers = { 45, 78, 985, 635, 74, 11, 6589, 4125, 2103698 };
+
+			//foreach (int number in numbers) 
+			//{
+			//	if (number % 2 == 0)
+			//	{  Console.WriteLine(number); }
+
+			//}
+
+
+			//int[] numbers = { 45, 78, 985, 635, 74, 11, 6589, 4125, 2103698 };
+			//int total = 0;
+
+			//foreach (int number in numbers)
+			//{
+			//	total += number;
+			//}
+			//Console.WriteLine(total);
+
+
+			//List<int> numbers = new List<int>()
+			//{
+			//	1,2,3,4,5,6
+			//};
+			//foreach (int number in numbers)
+			//{
+			//	Console.WriteLine(number);
+			//}
+
+
+			//string word = "merhaba";
+
+			//foreach  (char c in word)
+			//{
+			//	Console.WriteLine(c);
+			//}
+
+			#region Örnek Sınav Sistemi Uygulaması 
+//			Console.Write("*********C# Eğitim kampı Sınav Uygulaması");
+//				Console.WriteLine();
+//				Console.WriteLine();
+//				Console.WriteLine();
+//				Console.WriteLine();
+//				Console.WriteLine();
+
+//			//sınıftaki çğrenci sayısını kullanıcıan alma
+//			Console.WriteLine("-------------------------------------");
+//				Console.Write("Sınıfınızda Kaç Öğrenci Var ?");
+//			int studentCount=int.Parse(Console.ReadLine());
+//			Console.WriteLine("--------------------------");
+
+//			//Öğrenci isimlerini ve not ortalamalarını saklayacak diziler
+//			string[] studentCountNames = new string[studentCount];
+//			double[] studentExamAvg = new double[studentCount];
+
+//			for (int i =0; i<studentCount; i++)
+//			{
+//				Console.Write($"{i + 1} . öğrencinin ismini giriniz:   ")
+//;
+//				studentCountNames[i] = Console.ReadLine();
+
+//				double totalExamResult = 0;
+
+//				// her öğrenci için 3 sınav notu girişi 
+
+//				for (int j = 0; j < 3; j++)
+//				{
+//					Console.Write($"{studentCountNames[i]} isimli öğrencinin {j + 1}. sınav notunu giriniz:  ");
+//					double value = double.Parse(Console.ReadLine());
+//					totalExamResult += value;// notları tolama kısmı
+
+//				}
+
+//				studentExamAvg[i] = totalExamResult/3;
+//			}
+
+//			//sınav oratalamaları 
+//			for (int i = 0; i < studentCount; i++)
+//			{
+//				Console.WriteLine($"{ studentCountNames[i]} adlı öğrencinin ortalaması: { studentExamAvg[i]}");
+
+
+//				//Öğrenciilerin geçip kalma durumları
+//				if (studentExamAvg[i]>=50)
+//				{
+//					Console.WriteLine($"{studentCountNames[i]} adlı öğrenci dersi geçti");
+//				}
+//				else
+//				{
+
+//					Console.WriteLine($"{studentCountNames[i]} adlı öğrenci dersten kaldı");
+//				}
+
+//				Console.WriteLine("-----------------------------------------------");
+//			}
+
+
+
+
+			#endregion
+
+
+			Console.ReadLine();
+
+			#endregion
+
+
+		}
+	}
+}
---
+++ 08_Metods/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Metods
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Void Metotlar
+			//()
+			//Geriye Değer Döndürmeyen Metotlar
+			//Customer --> Lİstele,ekle,sil,güncelle
+			//void anahtar kelimesi
+
+
+			//void CustomerList()
+			//{
+
+			//	Console.WriteLine("Ali Yıldız");
+			//	Console.WriteLine("Ayşe Yıldız");
+			//	Console.WriteLine("Hakan Öztürk");
+			//	Console.WriteLine("Merve Çınar");
+			//}
+			//CustomerList();
+			//CustomerList();
+			//CustomerList();
+			//CustomerList();
+
+
+			//	void sum()
+			//{
+			//	int x = 1;
+			//	int y=2;
+			//	int z = x + y;
+			//	Console.WriteLine(z);
+			//}
+
+			//sum();
+
+			#endregion
+
+			#region Geriye Değer Döndürmeyen string PArametreli Metotlar
+
+
+			//void WriteMethod(string customerNmae)
+			//{
+			//	Console.WriteLine(customerNmae);
+
+			//}
+			//WriteMethod("Mehmet Yıldırım");
+
+			// void customerCart (string name,string surname)
+			//{
+
+			//	Console.WriteLine("müşteri: " + name + " " + surname);
+			//}
+
+			//customerCart("Mehmet", "Yıldız");
+			//customerCart("Ayşegül"," Kaya");
+			#endregion
+
+
+
+			#region  Geriye Değer Döndürmeye Parametreli Metotlar
+
+			//void sum(int number1, int number2, int number3)
+			//{
+
+			//	int result = number1 + number2 + number3;
+			//		Console.WriteLine(result);
+
+			//}
+			//sum(4, 6, 45);
+			#endregion
+
+
+			#region Geriye DEğer Döndüren Metotlar
+
+			//string CustomerName()
+			//{ return "hürrem sultan ";
+			//}
+
+			//CustomerName();
+
+
+
+			//string studentCard ()
+			//{
+			//	string name = "ali";
+			//	string surName = "Yıldız";
+
+			//	return name + " " + " " + surName;
+			//}
+
+			//Console.WriteLine(studentCard ());
+
+
+
+
+
+			#endregion
+
+
+			#region #region Geriye DEğer Döndüren  String Parametreli Metotlar
+
+			//		string CountryCard (string countryName, string capital , string flagColor) 
+			//		{
+			//			string cardInfo = "Ülke" + countryName + " - Başkent "
+			//+ capital + "- Bayrak Rengi" + flagColor;
+			//			return cardInfo;
+			//		}
+
+			//		string x, y, z;
+			//		Console.Write("Ülke Adını Giriniz:    ");
+			//		x= Console.ReadLine();
+			//		Console.Write("Ülke Başkentini Giriniz:    ");
+			//		y= Console.ReadLine();
+			//		Console.WriteLine("ülke bayrak rengini giriniz");
+			//		z= Console.ReadLine();
+
+			//		Console.WriteLine(CountryCard(x, y, z));
+
+			//		Console.WriteLine(CountryCard("türkiye", "Ankara", "Kırmızı- Beyaz"));
+
+
+
+			#endregion
+
+
+			#region #region Geriye DEğer Döndüren  Int PArametreli Metotlar
+
+
+   //         int Sum(int number1, int number2) 
+			//{
+			//int result =number1 + number2;
+			//	return result;
+			
+			//}
+			//Console.WriteLine(Sum(45,90));
+			//Console.WriteLine(Sum(36,25));
+			//Console.WriteLine(Sum(44,36));
+			//Console.WriteLine(Sum(14,20));
+
+
+		
+
+		
+			#endregion
+
+
+				 string ExamResult(string student ,int exam1, int exam2,int exam3) 
+			     {
+			   int result=(exam1+ exam2+ exam3)/3;
+				if (result >= 50)
+				{
+					return  student + "  İsimli Öğrenci Geçti  " +	"Ortalama: "+ result;
+				}
+                else
+                {
+					return student + "  İsimli Öğrenci Kaldı  " + "Ortalama: " + result;
+				}
+
+                 } 
+			Console.WriteLine(ExamResult("Ali", 25, 41, 85));
+			Console.WriteLine(ExamResult("Ayşe", 36, 88, 33));
+
+			Console.ReadLine();
+
+
+		}
+	}
+}
